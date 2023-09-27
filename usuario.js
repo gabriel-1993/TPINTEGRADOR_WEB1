2bf0148d1df097d6812b9f370d31e115033b472b
Gabriel Torrez <110358252+gabriel-1993@users.noreply.github.com>
2023-09-27 15:03:22 -0300

    formIngresoOkCardMsjsOk

--- usuario.js
+++ usuario.js
@@ -2,36 +2,126 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     const formIngresar = document.getElementById("ingresoForm");
-    const nombreIngresar = document.getElementById("nombreIngresar");
+    const usuarioIngresar = document.getElementById("nombreIngresar");
+    const passwordIngresar = document.getElementById("passwordIngresar");
     const divMsj = document.querySelector(".divMensaje");
     const btnIngresar = document.querySelector(".btnIngresar");
     const btnMsjAceptar = document.querySelector(".btnAceptar");
 
+    //MENSAJE DE ERROR
     // CreateElement  p: msj de error dentro del div
     var pMsj = document.createElement("p");
-    //Agrego class pMsj para el css
+    //Agrego class pMsj para mostrar con el css
     pMsj.className = "pMsj";
     //Agrego el texto del p
     pMsj.textContent = "Por favor, ingrese un nombre de usuario";
     //guardo dentro del divMsj el pMsj
     divMsj.appendChild(pMsj);
+    //logo de error para agregar al msj
+    const imgError = document.createElement("img");
+    imgError.className = "imgError";
+    imgError.src = "./imagenesChicas/error.png";
+
+
+    //Expresion regular para usuario
+    const usuarioExp = /^[a-zA-Z0-9]{6,12}$/;
+
+    let usuarioCumple = false;
 
     formIngresar.addEventListener("submit", function (event) {
         event.preventDefault();
-        const nombre = nombreIngresar.value.trim();
-        if (nombre === "") {
-            // El campo esta vacio, mostramos el mensaje de error
+
+
+
+        //VALIDAR USUARIO 
+
+        // guardar usuario ingresado
+        const usuario = usuarioIngresar.value.trim();
+
+        if (usuario === "") {
+            // msj de error para campo vacio
+            pMsj.style.fontSize="1.3rem";            
+            pMsj.textContent = "Error: El campo de usuario está vacío.";
+            pMsj.appendChild(imgError);
             divMsj.classList.add("mostrarMsj");
-            if (btnMsjAceptar.addEventListener('click', function () {
-                divMsj.classList.remove("mostrarMsj");
-            }));
+        } else if (!usuarioExp.test(usuario)) {
+            // El usuario no cumple con los criterios letras azAZ09, min 6 letras max 12, muestra el mensaje de error
+            pMsj.appendChild(imgError);
+            pMsj.textContent = "Error: El usuario puede combinar letras y números, mínimo 6 caracteres y máximo 12.";
+            divMsj.classList.add("mostrarMsj");
         } else {
-            // El campo se ha llenado, ocultamos el mensaje de error
-            divMsj.classList.remove("mostrarMsj");
-            divMsj.innerHTML = ""; // Limpiamos el contenido del div
+            // Usuario valido guardo boolean para comparar al final
+            usuarioCumple = true;
         }
+        // el evento click de la card con msjs esta luego del password
+
+
+
+
+        //VALIDAR PASSWORD
+
+        //guardar password
+        //Expresion regular para pass
+        const passwordExp = /^[a-zA-Z0-9]{5,10}$/;
+        const password = passwordIngresar.value.trim();
+        let passwordCumple = false;
+
+
+
+        if (password === "") {
+            // msj de error para campo vacio
+            pMsj.style.fontSize="1.3rem";
+            pMsj.textContent = "Error: El campo de password está vacío.";
+            pMsj.appendChild(imgError);
+            btnMsjAceptar.classList.add("hoverError");
+            divMsj.classList.add("mostrarMsj");
+        } else if (!passwordExp.test(password)) {
+            // El usuario no cumple con los criterios letras azAZ09, min 5 letras max 10, muestra el mensaje de error
+            pMsj.style.fontSize="1.3rem";
+            pMsj.textContent = "Error: El password puede combinar letras y números, mínimo 5 caracteres y máximo 10.";
+            pMsj.appendChild(imgError);
+            btnMsjAceptar.classList.add("hoverError");
+            divMsj.classList.add("mostrarMsj");
+        } else {
+            // Usuario valido guardo boolean para comparar al final
+            passwordCumple = true;
+        }
+        //si ambos cumplen mostrar msj,limpiar campos y enviar form
+        //imagen de Ok para ingresar cuando cumple:
+        const imgOk = document.createElement("img");
+        imgOk.className = "imgOk";
+        imgOk.src = "./imagenesChicas/ok.png";
+
+        if (usuarioIngresar.value === "" && passwordIngresar.value === "") {
+            pMsj.textContent = "Datos vacios, ingrese usuario y password";
+            pMsj.appendChild(imgError);
+            btnMsjAceptar.classList.add("hoverError");
+            divMsj.classList.add("mostrarMsj");
+        }
+
+        if (usuarioCumple && passwordCumple) {
+            //limpiar campos
+            usuarioIngresar.value = "";
+            passwordIngresar.value = "";
+            //mostrar msj
+            pMsj.textContent = "¡Iniciando sesión!";
+            pMsj.appendChild(imgOk);
+            divMsj.classList.add("mostrarMsj");
+
+
+        }
+
+        // Evento click para la card de mensajes
+        btnMsjAceptar.addEventListener('click', function () {
+            usuarioIngresar.value = "";
+            passwordIngresar.value = "";
+            divMsj.classList.remove("mostrarMsj");
+            // Enviar el formulario
+            formIngresar.submit();
+        });
+
+
     });
-    console.log(SubmitEvent);
 
 
 
