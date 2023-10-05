a4a081f5a08e41706f3b60433b8e48276ff1309a
Gabriel Torrez <110358252+gabriel-1993@users.noreply.github.com>
2023-10-04 21:09:10 -0300

    agregoImagenCarrousel
    responsiveCelVertial

--- carrousel.js
+++ carrousel.js
@@ -1,60 +1,46 @@
-// CARROUSEL  ***********************************************************************************************************
-
 document.addEventListener("DOMContentLoaded", () => {
-
-// Lista con rutas de imagenes
-const listaImagenes = [
-    './imagenesGrandes/1.jpg',
-    './imagenesGrandes/2.jpg',
-    './imagenesGrandes/3.jpg',
-    './imagenesGrandes/4.jpg',
-    './imagenesGrandes/5.jpg',
-    './imagenesGrandes/6.jpg',
-    './imagenesGrandes/7.jpg',
-    './imagenesGrandes/8.jpg',
-    './imagenesGrandes/9.jpg',
-    './imagenesGrandes/10.jpg',
-    './imagenesGrandes/11.jpg'
-];
-
-const etiquetaImagen = document.getElementById('imagen');
-const btnAnterior = document.querySelector('.btnAnterior');
-const btnSiguiente = document.querySelector('.btnSiguiente');
-
-let indexImagen = 0;
-
-// Funcion para mostrar la imagen actual
-function mostrarImagen() {
-    const imagenActual = listaImagenes[indexImagen];
-    etiquetaImagen.src = imagenActual;
-}
-// Incrementa el valor de indexImagen en 1 y utiliza el operador % para asegurarse de que el indice este dentro de los valores permitidos de la lista
-function mostrarSiguiente() {
-    indexImagen = (indexImagen + 1) % listaImagenes.length;
-    mostrarImagen();
-}
-
-//Disminuye el valor de indexImagen en 1 y utiliza el operador % para asegurarse 
-//de que el indice este dentro de los valores permitidos de la lista
-// Funcion para mostrar la imagen anterior
-function mostrarAnterior() {
-    indexImagen = (indexImagen - 1 + listaImagenes.length) % listaImagenes.length;
-    mostrarImagen();
-}
-
-// Mostrar la imagen inicial
-mostrarImagen();
-
-// Agregar escuchadores de eventos a los botones para cambiar de imagenes
-btnSiguiente.addEventListener('click', mostrarSiguiente);
-btnAnterior.addEventListener('click', mostrarAnterior);
-
-
-
-    // llave,parentesis y ; del document.addEventListener("DOMContentLoaded", () => {  Principal para 
-    // ejecutar el js una vez este todo el html cargado...
-});
-
-
-
-
+    const etiquetaImagen = document.getElementById('imagen');
+    const btnAnterior = document.querySelector('.btnAnterior');
+    const btnSiguiente = document.querySelector('.btnSiguiente');
+  
+    let indexImagen = 0;
+    let listaImagenes = []; // Declarar listaImagenes aquí para tener alcance global
+  
+    function mostrarImagen() {
+      let imagenActual = listaImagenes[indexImagen];
+      etiquetaImagen.src = imagenActual;
+    }
+  
+    function mostrarSiguiente() {
+      indexImagen = (indexImagen + 1) % listaImagenes.length;
+      mostrarImagen();
+    }
+  
+    function mostrarAnterior() {
+      indexImagen = (indexImagen - 1 + listaImagenes.length) % listaImagenes.length;
+      mostrarImagen();
+    }
+  
+    function actualizarListaImagenes() {
+      const screenWidth = window.innerWidth;
+      let imagenesGrandes = ['./imagenesGrandes/1.jpg', './imagenesGrandes/2.jpg', /* ... */];
+      let imagenesChicas = ['./imagenesChicas/1.jpg', './imagenesChicas/2.jpg', /* ... */];
+  
+      if (screenWidth <= 576) {
+        listaImagenes = imagenesChicas.slice();
+      } else {
+        listaImagenes = imagenesGrandes.slice();
+      }
+  
+      mostrarImagen(); // Actualiza la imagen cuando cambia el tamaño de la pantalla
+    }
+  
+    // Llama a actualizarListaImagenes al principio para inicializar la lista
+    actualizarListaImagenes();
+  
+    // Agregar escuchadores de eventos al cambio de tamaño de la ventana
+    window.addEventListener('resize', actualizarListaImagenes);
+    // Agregar escuchadores de eventos a los botones para cambiar de imágenes
+    btnSiguiente.addEventListener('click', mostrarSiguiente);
+    btnAnterior.addEventListener('click', mostrarAnterior);
+  });
