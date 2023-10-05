12ca5636806a9dfa4fb32a73689f48e89172d9f4
Gabriel Torrez <110358252+gabriel-1993@users.noreply.github.com>
2023-10-04 21:31:28 -0300

    actualizoImgParaCel

--- carrousel.js
+++ carrousel.js
@@ -1,66 +1,66 @@
-',
-      './imagenesGrandes/7.jpg',
-      './imagenesGrandes/8.jpg',
-      './imagenesGrandes/9.jpgdocument.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     const etiquetaImagen = document.getElementById('imagen');
     const btnAnterior = document.querySelector('.btnAnterior');
     const btnSiguiente = document.querySelector('.btnSiguiente');
-  
+
     let indexImagen = 0;
     let listaImagenes = []; // Declarar listaImagenes aquí para tener alcance global
-  
+
     function mostrarImagen() {
-      let imagenActual = listaImagenes[indexImagen];
-      etiquetaImagen.src = imagenActual;
+        let imagenActual = listaImagenes[indexImagen];
+        etiquetaImagen.src = imagenActual;
     }
-  
+
     function mostrarSiguiente() {
-      indexImagen = (indexImagen + 1) % listaImagenes.length;
-      mostrarImagen();
+        indexImagen = (indexImagen + 1) % listaImagenes.length;
+        mostrarImagen();
     }
-  
+
     function mostrarAnterior() {
-      indexImagen = (indexImagen - 1 + listaImagenes.length) % listaImagenes.length;
-      mostrarImagen();
+        indexImagen = (indexImagen - 1 + listaImagenes.length) % listaImagenes.length;
+        mostrarImagen();
     }
-  
+
     function actualizarListaImagenes() {
-      const screenWidth = window.innerWidth;
-      let imagenesGrandes = [  './imagenesGrandes/1.jpg',
-      './imagenesGrandes/2.jpg',
-      './imagenesGrandes/3.jpg',
-      './imagenesGrandes/4.jpg',
-      './imagenesGrandes/5.jpg',
-      './imagenesGrandes/6.jpg',
-      './imagenesGrandes/10.jpg',
-      './imagenesGrandes/11.jpg'  ];
-      let imagenesChicas = [  './imagenesChicas/1.jpg',
-      './imagenesChicas/2.jpg',
-      './imagenesChicas/3.jpg',
-      './imagenesChicas/4.jpg',
-      './imagenesChicas/5.jpg',
-      './imagenesChicas/6.jpg',
-      './imagenesChicas/7.jpg',
-      './imagenesChicas/8.jpg',
-      './imagenesChicas/9.jpg',
-      './imagenesChicas/10.jpg',
-      './imagenesChicas/11.jpg'  ];
-  
-      if (screenWidth <= 391) {
-        listaImagenes = imagenesChicas.slice();
-      } else {
-        listaImagenes = imagenesGrandes.slice();
-      }
-  
-      mostrarImagen(); // Actualiza la imagen cuando cambia el tamaño de la pantalla
+        const screenWidth = window.innerWidth;
+        let imagenesGrandes = ['./imagenesGrandes/1.jpg',
+            './imagenesGrandes/2.jpg',
+            './imagenesGrandes/3.jpg',
+            './imagenesGrandes/4.jpg',
+            './imagenesGrandes/5.jpg',
+            './imagenesGrandes/6.jpg',
+            './imagenesGrandes/10.jpg',
+            './imagenesGrandes/11.jpg'];
+
+        let imagenesChicas = ['./imagenesChicas/1.jpg',
+            './imagenesChicas/2.jpg',
+            './imagenesChicas/3.jpg',
+            './imagenesChicas/4.jpg',
+            './imagenesChicas/5.jpg',
+            './imagenesChicas/6.jpg',
+            './imagenesChicas/7.jpg',
+            './imagenesChicas/8.jpg',
+            './imagenesChicas/9.jpg',
+            './imagenesChicas/10.jpg',
+            './imagenesChicas/11.jpg'];
+
+        if (screenWidth <= 391) {
+            listaImagenes = imagenesChicas.slice();
+        } else {
+            listaImagenes = imagenesGrandes.slice();
+        }
+
+        mostrarImagen(); // Actualiza la imagen cuando cambia el tamaño de la pantalla
     }
-  
+
     // Llama a actualizarListaImagenes al principio para inicializar la lista
     actualizarListaImagenes();
-  
+
     // Agregar escuchadores de eventos al cambio de tamaño de la ventana
     window.addEventListener('resize', actualizarListaImagenes);
     // Agregar escuchadores de eventos a los botones para cambiar de imágenes
     btnSiguiente.addEventListener('click', mostrarSiguiente);
     btnAnterior.addEventListener('click', mostrarAnterior);
-  });
+
+});
+
